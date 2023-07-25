966710d5863867fe5371658da02a839984be705f
Matthew Lonis <mattlonis@gmail.com>
2023-07-25 15:24:14 -0400

    Update LineSegment.java

--- java-line-sweeper/src/main/java/com/mrlonis/LineSegment.java
+++ java-line-sweeper/src/main/java/com/mrlonis/LineSegment.java
@@ -1,6 +1,9 @@
 package com.mrlonis;
 
-import java.awt.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
