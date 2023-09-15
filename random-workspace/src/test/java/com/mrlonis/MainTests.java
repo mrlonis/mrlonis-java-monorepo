7acef79fa38fb4ae8a7449bfafecb2603cee22bc
Matthew Lonis <mattlonis@gmail.com>
2023-09-15 11:45:06 -0400

    Adding new workspace

---
+++ random-workspace/src/test/java/com/mrlonis/MainTests.java
@@ -0,0 +1,13 @@
+package com.mrlonis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTests {
+    @Test
+    void testMain() {
+        Main.main(new String[]{});
+        assertTrue(true);
+    }
+}
